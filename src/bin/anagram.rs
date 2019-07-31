3665dd58ff3113239132c2d8dcb0eaccd88ac98d
Stefano Salvagni <ste0891@gmail.com>
2019-07-31 14:53:14 +0200

    divide anagram app in function, handle errors

--- src/bin/anagram.rs
+++ src/bin/anagram.rs
@@ -1,31 +1,49 @@
 use std::io::{prelude::*};
+use std::io::Lines;
+use std::io::Result;
 use std::iter::FromIterator;
+use std::fs::File;
+use std::io::BufReader;
+use std::error::Error;
 
 fn main() {
     println!("Anagram app!");
 
     let mut word = String::new();
 
-    println!("Your word: ");
+    print!("Your word: \n");
     std::io::stdin().read_line(&mut word).expect("Failed to read the word!");
-    println!("You typed: {}", word);
     let mut sorted_word: Vec<char> = word.trim().chars().collect();
     sorted_word.sort_by(|a,b| a.cmp(b));
     word = String::from_iter(sorted_word);
 
-    //unwrap(): not explicitly handling error
-    let _f = std::fs::File::open("src/bin/dictionary.txt").unwrap();
-    let lines = std::io::BufReader::new(_f).lines();
+    println!("{:?}", find_anagram(word));
+}
+
+fn find_anagram(w: String) -> Vec<String> {
+
     let mut anagrams: Vec<String> = Vec::new();
-    for line in lines {
-        let l = line.unwrap();
-        let mut chs: Vec<char> = l.chars().collect();
-        chs.sort_by(|a, b| a.cmp(b));
-        let s = String::from_iter(chs);
-        if s==word {
-            anagrams.push(l);
+    if let Ok(lines) = read_lines("src/bin/dictionary.txt") {
+        for line in lines {
+            if let Ok(current_word) = line {
+                let mut chs: Vec<char> = current_word.chars().collect();
+                chs.sort_by(|a, b| a.cmp(b));
+                let s = String::from_iter(chs);
+                if s==w {
+                    anagrams.push(current_word);
+                }
+            }
         }
     }
+    anagrams
+}
 
-    println!("{:?}", anagrams);
+fn read_lines(path: &str) -> Result<Lines<BufReader<File>>> {
+    //unwrap(): not explicitly handling error
+    let mut _f = match File::open(path) {
+        Err(e) => panic!("Error opening file {}: {}", path, e.description()),
+        Ok(file) => file,
+    };
+
+    Ok(BufReader::new(_f).lines())
 }
