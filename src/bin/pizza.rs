a5cfae30309149e0f2754f11758357ee99f62e2a
Stefano Salvagni <ste0891@gmail.com>
2019-08-12 15:26:04 +0200

    two types of Pizza with common Pizzable trait and Basket with a vector of Pizzable

--- src/bin/pizza.rs
+++ src/bin/pizza.rs
@@ -1,39 +1,95 @@
 //the file itself acts as a module: no need to mod declaration!
-pub struct Pizza {
-    pub name: String,
-    pub price: f64
+struct Pizza {
+    name: String,
+    price: f64
 }
-trait Calculable {
+
+impl Pizza {
+    fn new(name: String, price: f64) -> Self {
+        Pizza {
+            name: name,
+            price: price
+        }
+    }
+
+    fn name(self) -> String {
+        self.name
+    }
+
+    fn price(self) -> f64 {
+        self.price
+    }
+}
+
+trait Pizzable {
     fn get_price(&self) -> f64;
 }
-impl Calculable for Pizza {
+
+impl Pizzable for Pizza {
     fn get_price(&self) -> f64 {
         self.price
     }
 }
-pub struct PizzaWithAddon {
-    name: String,
-    pub pizza: Pizza,
+
+struct PizzaWithAddon {
+    name_addon: String,
+    pizza: Pizza,
 }
-impl Calculable for PizzaWithAddon {
+
+impl PizzaWithAddon {
+    fn new(name: String, price: f64, name_addon: String) -> Self {
+        PizzaWithAddon {
+            name_addon: name_addon,
+            pizza: Pizza {
+                name: name,
+                price: price
+            }
+        }
+    }
+}
+
+impl Pizzable for PizzaWithAddon {
     fn get_price(&self) -> f64 {
         self.pizza.get_price() + 0.50
     }
 }
-pub enum PizzasTypes {
-    Pizza,
-    Diavola
+
+struct Basket {
+    pizzas: Vec<Box<Pizzable>>
+}
+
+trait Summable {
+    fn get_sum(self) -> f64;
+}
+
+impl Summable for Basket {
+    fn get_sum(self) -> f64 {
+        let mut tot: f64 = 0.0;
+        for pizza in self.pizzas {
+            tot = tot + pizza.get_price();
+        }
+        tot
+    }
+}
+
+fn print_basket(basket: Basket) {
+    println!("Basket------------");
+    for pizza in basket.pizzas {
+        println!("");
+    }
+}
+
+fn list_of_pizzas() {
+
 }
 
 fn main() {
-    let p = Pizza {
-        name: "Diavola".to_string(),
-        price: 8.34
+    let pm = Pizza::new("Margherita".to_string(), 6.10);
+    let pa = PizzaWithAddon::new("Diavola".to_string(), 8.34, "ketchup".to_string());
+    let mut bask = Basket {
+        pizzas: Vec::new()
     };
-    println!("{}",p.get_price());
-    let pa = PizzaWithAddon {
-        name: "mayonnese".to_string(),
-        pizza: p
-    };
-    println!("{}", pa.get_price());
+    bask.pizzas.push(Box::new(pm));
+    bask.pizzas.push(Box::new(pa));
+    println!("{}", bask.get_sum());
 }
